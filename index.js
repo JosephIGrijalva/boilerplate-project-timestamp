f29e28c4cac7979e86b7b37c6d738e5db7466be0
Joseph Grijalva <jgrijalva580@gmail.com>
2022-06-07 07:10:48 -0500

    fixed the first api endpoint

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:timestamp", function (req, res) {
+  let time = req.params.timestamp;
+  let date = new Date(time);
+  let unixTime = date.UTC();
+  let dateString = date.toGMTString();
+  res.json({
+    unix: unixTime,
+    utc:dateString
+  })
 });
 
 
